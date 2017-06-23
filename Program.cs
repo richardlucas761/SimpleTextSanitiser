e74f8884a0cb2ba40732b6f1e8684825eaff83f7
Richard Lucas <sherlockdoge@gmail.com>
2017-06-23 08:51:20 +0100

    Fix invalid tests. Add HTML Agility Pack package. Implement functionality to satisfy unit tests.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,55 @@
-﻿namespace SimpleTextSanitiser
+﻿using System;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace SimpleTextSanitiser
 {
     public class Program
     {
+        private const string NoInputFoundToSanitise = "No input found to sanitise.";
+        private const string ScriptNodeName = "script";
+
         // ReSharper disable once UnusedParameter.Local
         private static void Main(string[] args)
         {
+            // Read in text input from standard input
+            var inputText = new StringBuilder();
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != null)
+            {
+                inputText.AppendLine(inputLine);
+                inputLine = Console.ReadLine();
+            }
+
+            // Validate the input
+            if (inputText == null) throw new Exception(NoInputFoundToSanitise);
+
+            // Write sanitised output to standard output
+            var outputText = SanitiseInput(inputText);
+            Console.Write(outputText);
         }
 
-        public string SanitiseInput(string inputString)
+        public static string SanitiseInput(StringBuilder inputString)
         {
-            return "";
+            if (inputString == null) throw new ArgumentException(nameof(inputString));
+
+            // Parse the input text using HTML Agility Pack
+            var doc = new HtmlDocument();
+            doc.LoadHtml(inputString.ToString());
+
+            // Read all of the nodes from the document which aren't a script
+            var nodes = from getnode in doc.DocumentNode.ChildNodes where getnode.Name.ToLowerInvariant() != ScriptNodeName select getnode;
+
+            // Rebuild the text from the collection and return it
+            var outputStringBuilder = new StringBuilder();
+            foreach (var node in nodes)
+            {
+                outputStringBuilder.Append(node.InnerText);
+            }
+
+            return outputStringBuilder.ToString();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SimpleTextSanitiser
+
 A console application to carry out a simple sanitisation of input text.
+
+## Usage
+
+Reads in text from standard input and outputs a sanitised version to standard output.
+
+_Currently only processes and removes \<script\> tags from text_
